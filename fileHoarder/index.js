1cfd4e5da42a1353504ac4232a48d8ed26ccd3ea
Nathan Pope <the@programmingpope.com>
2019-01-24 20:04:16 -0700

    Add filehoarder

---
+++ fileHoarder/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const fileUpload = require('express-fileupload')
+const filenamify = require('filenamify')
+const fs = require('fs');
+const targetDir = "uploads";
+
+if (!fs.existsSync(targetDir)){
+    fs.mkdirSync(targetDir);
+}
+
+app.use(fileUpload());
+
+app.post('/', (req, res) => {
+    let email = filenamify(req.body.email, {replacement: '_'});
+
+    let requestDir = `${targetDir}/${email}`;
+    if (!fs.existsSync(requestDir)){
+        fs.mkdirSync(requestDir);
+    }
+    
+    let file = req.files.file;
+    file.mv(`./${requestDir}/${file.name}`, (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send(err);
+            return;
+        }
+        console.log("completed");
+        res.send("ok");
+    });
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}!`);
+});
---
+++ fileHoarder/readme.md
@@ -0,0 +1,5 @@
+running from windows:
+```
+docker run --name filehoarder -p 3000:3000 -d --rm -v C:\Users\pr
+ogrammingpope\Documents\Dev\fileHoarder\uploads:/usr/src/app/uploads filehoarder
+```
