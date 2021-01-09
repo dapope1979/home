9764c05508d386acb993e627cab7a6bcce53242c
Nathan Pope <dapope1979@gmail.com>
2021-01-09 10:55:54 -0700

    Update startContainers.sh

--- startContainers.sh
+++ startContainers.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # minecraft
-docker-compose -f minecraft/creative.yml  up -d
 docker-compose -f minecraft/survival.yml up -d
-#docker-compose -f minecraft/modded.yml up -d
 
 # plex
 docker-compose -f plex/docker-compose.yml up -d
