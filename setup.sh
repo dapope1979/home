e2343daffc013905fbc5ce21f334d8b169abf32f
Nathan Pope <dapope1979@gmail.com>
2021-01-09 10:05:19 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 apt-get update
+apt-get install zip -y
 
 #docker
-apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 curl -fsSl https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
@@ -23,6 +24,8 @@ ufw allow 25567
 ufw allow 32400
 
 #pihole - disable DNS stub listener & use DNS from DHCP
+ufw allow 80
+ufw allow 443
 sudo sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
 sudo sh -c 'rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf'
 
