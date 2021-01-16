d5d39ba350cecaf72b4d20954a6bb1bd09c1fc87
Nathan Pope <dapope1979@gmail.com>
2021-01-16 15:45:12 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,16 +4,23 @@ apt-get update
 apt-get install zip -y
 
 #docker
-apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
-curl -fsSl https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
+   
 apt-get update
-apt-get install docker-ce docker-compose -y
-usermod -aG docker vatican
-docker network create vatican-bridge
+apt-get install docker-ce docker-ce-cli containerd.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 #minecraft firewall rules
 ufw allow 25565
@@ -26,7 +33,5 @@ ufw allow 32400
 #pihole - disable DNS stub listener & use DNS from DHCP
 ufw allow 80
 ufw allow 443
-sudo sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
-sudo sh -c 'rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf'
-
-echo "Remember to set $PIHOLE_PASSWORD
+sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
+sh -c 'rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf'
