fed815cbf85ae2a4d5ce9ab7b8768aa494740e55
Nathan Pope <dapope1979@gmail.com>
2021-01-16 21:21:34 -0700

    Update startContainers.sh

--- startContainers.sh
+++ startContainers.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# pihole
+docker-compose -f pihole/docker-compose.yml up -d
+
 # minecraft
 docker-compose -f minecraft/survival.yml up -d
 
 # plex
 docker-compose -f plex/docker-compose.yml up -d
 
-# fileHoarder for plex channels
+# samba for windows shares
 docker-compose -f fileHoarder/docker-compose.yml up -d
