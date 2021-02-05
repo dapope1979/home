12ea88ae68531d5c51a0e77362c8c8ed340ab8f9
Nathan Pope <dapope1979@gmail.com>
2021-02-04 21:01:38 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,27 +3,7 @@
 apt-get update
 apt-get install zip -y
 
-#docker
-apt-get remove docker docker-engine docker.io containerd runc
-apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common    
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-   
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-usermod -aG docker vatican
-docker network create vatican-bridge
+sudo snap install docker
 
 #minecraft firewall rules
 ufw allow 25565
@@ -36,5 +16,5 @@ ufw allow 32400
 #pihole - disable DNS stub listener & use DNS from DHCP
 ufw allow 80
 ufw allow 443
-sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
-sh -c 'rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf'
+#sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
+#sh -c 'rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf'
