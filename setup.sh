81bff1a3d148425a37af1dc48f2ef9fbfbdd66cb
Nathan Pope <dapope1979@gmail.com>
2021-02-04 22:19:26 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,12 +6,31 @@ apt-get upgrade
 # Network
 rm /etc/netplan/*
 cp 01-netcfg.yaml /etc/netplan/
-
-sudo snap install docker
+csed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
+sh -c 'rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf'
+netplan generate
+netplan apply
 
 #ZFS
 apt install zfsutils-linux
 zpool import -f well
+
+# Docker
+apt-get remove -y docker docker-engine docker.io containerd runc
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+usermod -aG docker vatican
 
 #minecraft firewall rules
 ufw allow 25565
@@ -24,5 +43,4 @@ ufw allow 32400
 #pihole - disable DNS stub listener & use DNS from DHCP
 ufw allow 80
 ufw allow 443
-#sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
-#sh -c 'rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf'
+
