c296663cd25f093e98edb54b919eb315e7669f25
Nathan Pope <dapope1979@gmail.com>
2021-02-04 22:54:01 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ apt-get upgrade
 # Network
 rm /etc/netplan/*
 cp 01-netcfg.yaml /etc/netplan/
-csed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
+sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
 sh -c 'rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf'
 netplan generate
 netplan apply
