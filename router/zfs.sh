5c1275916ca799a6d8419591b440972e44d93e21
Nathan Pope <dapope1979@gmail.com>
2024-08-03 07:06:41 -0600

    zfs and central container storage

--- router/zfs.sh
+++ router/zfs.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 # https://wiki.debian.org/ZFS
-
+# attended install due to license warning
 codename=$(lsb_release -cs);echo "deb http://deb.debian.org/debian $codename-backports main contrib non-free"|tee -a /etc/apt/sources.list
 apt update
 apt install -y linux-headers-amd64
 apt install -y -t stable-backports zfsutils-linux
+modeprobe zfs
 
-# attended install due to license warning
-#apt install -y -t bullseye-backports zfsutils-linux
-#zpool import well
+echo "\n\nHere are the available disks:\n\n"
+fdisk -l
+
+echo "\n\nTrying to import pool\n\n"
+zpool import well
+
