6a4fa4e5843b535598cc7e01832ed4dca53cd950
Nathan Pope <dapope1979@gmail.com>
2024-08-02 19:10:03 -0600

    Reorganize

--- network.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp interfaces /etc/network
-cp sysctl.conf /etc
-cp iptables-rules /etc/network
-cp iptables /etc/network/if-pre-up.d
-chmod 755 /etc/network/if-pre-up.d/iptables
-/etc/network/if-pre-up.d/iptables
-systemctl restart networking
