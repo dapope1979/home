cd987991223d454cbd7dc011e7473d77153adee2
Nathan Pope <dapope1979@gmail.com>
2024-08-03 12:27:04 -0600

    disable older network

--- router/networking.sh
+++ router/networking.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+rm /etc/network/interfaces
+systemctl disable --now networking.service
+
 cp networking/* /etc/systemd/network
 systemctl enable --now systemd-networkd.service
-
-echo "** Reboot required **"
