76c45821d457979a9cb1db869934f2ea924a8add
Nathan Pope <dapope1979@gmail.com>
2024-08-07 18:12:23 -0600

    try to fix crontab line

--- router/20-backup.sh
+++ router/20-backup.sh
@@ -3,7 +3,8 @@ apt install -y duplicity
 mkdir -p /well/backups/server/router
 
 # backup conatainer data everyday @ 3:00am
-crontab -l | { cat; echo "0 3 0 0 0 duplicity /var/lib/container-data file:///well/backups/server/router/ --no-encryption"; } | crontab -
+line="0 3 0 0 0 duplicity /var/lib/container-data file:///well/backups/server/router/ --no-encryption"
+(crontab -u $(whoami) -l; echo "$line" ) | crontab -u $(whoami) -
 
 # restore a single directory
 # add --force to ovewrite existing, use --file-to-restore to limit scope
