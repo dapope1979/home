f1ac6c371ebae5a0e6f7de8143eff00b1fe2badc
Nathan Pope <programmingpope@gmail.com>
2024-10-05 21:45:18 -0600

    tweaking order

--- router/02-storage.sh
+++ router/02-storage.sh
@@ -4,6 +4,10 @@ source /usr/local/sbin/configs/.env
 
 printf "\nConfiguring disk $DISK to mount at startup.\n"
 
+mkdir -p /mnt/well
+
 cat <<EOF>> /etc/fstab
 UUID=$DISK /mnt/well btrfs defaults 0 0
 EOF
+
+mount -av
