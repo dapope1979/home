242c3f246171d270e9ca7fe5273bf51554cc57fa
Nathan Pope <programmingpope@gmail.com>
2024-10-12 09:56:34 -0600

    fix environment variable

--- router/50-proxy.sh
+++ router/50-proxy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 source /usr/local/sbin/configs/.env
+export DOMAIN=$DOMAIN
 
 dnf install -y nginx
 rm /etc/nginx/nginx.conf
-cp envsubst < conf/nginx.conf > /etc/nginx/nginx.conf
+envsubst < conf/nginx.conf > /etc/nginx/nginx.conf
 
 rm /etc/cockpit/cockpit.conf
 cp conf/cockpit.conf /etc/cockpit/cockpit.conf
